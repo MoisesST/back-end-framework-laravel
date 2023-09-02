5dc9c27def73c31b4e11508a180ec1f48b9e91bc
MoisesST <99568996+MoisesST@users.noreply.github.com>
2023-09-01 22:01:09 -0300

    added tests (#2)

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -6,11 +6,30 @@ use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_that_true_is_true(): void
-    {
-        $this->assertTrue(true);
-    }
+  /**
+   * A basic test example.
+   */
+  public function test_that_true_is_true(): void
+  {
+    $this->assertTrue(true);
+  }
+
+  public function test_is_even_number(): void
+  {
+    $number = 4;
+    $this->assertTrue($number % 2 === 0);
+  }
+
+  public function test_string_contains_substring(): void
+  {
+    $mainString = "Hello, World!";
+    $substring = "Hello";
+    $this->assertStringContainsString($substring, $mainString);
+  }
+
+  public function test_array_is_empty(): void
+  {
+    $array = [];
+    $this->assertEmpty($array);
+  }
 }
